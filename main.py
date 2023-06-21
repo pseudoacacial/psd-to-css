4978b060d07072fc285ce43f87916030b71aec1e
pseudoacacial <aibanzai@protonmail.com>
2023-06-21 15:02:04 +0200

    smart objects

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from psd_tools import PSDImage
-psd = PSDImage.open('test/video-parallel-banner_set.psd')
+# psd = PSDImage.open('test/video-parallel-banner_set.psd')
+psd = PSDImage.open('test/smart_objects.psd')
 # psd.composite().save('video-parallel-banner_set.png')
 from modules.parse import getCSS
 import nested_config
@@ -9,9 +10,13 @@ def config():
     pairs = []
     f = open('config', 'r')
     for x in f:
-        pairs.append([word.rstrip() for word in x.split(" ")])
+        pairs.append([word.strip() for word in x.split(",")])
     f.close()
     return pairs
+
+def nested_config():
+    elements = {}
+    f = open('nested_config', 'r')
 
 
 print(getCSS(psd, config()))
--- modules/parse.py
+++ modules/parse.py
@@ -1,18 +1,28 @@
+from psd_tools import PSDImage
+import os
 def getCSS(psd, config):
     css = ""
-    for artboard in psd:
-        css += ".b" + artboard.name + " {"
+    for group in psd:
+        css += ".b" + group.name + " {"
+        print(group.kind)
+        if(group.kind == 'smartobject'):
+            print("smartobject layer. opening...")
+            group.smart_object.save('temp')
+            group = PSDImage.open("temp")
         for [psd_name, selector] in config:
-            css += getElementCSS(psd_name, selector, artboard)
+            css += getElementCSS(psd_name, selector, group)
+        if os.path.exists("temp"):
+            os.remove("temp") # Delete file
         css += "\n}\n\n"
     return css
 
 def getElementCSS(psd_name, selector, parent):
+    style = ""
     if listMatches(parent, psd_name):
-        style = "\n" + selector + " {\n"
         print(psd_name)
         match = listMatches(parent, psd_name)[0]
-        style += f'left: {match.offset[0] - parent.offset[0]}px;\n'\
+        style += "\n" + selector + " {\n"\
+            + f'left: {match.offset[0] - parent.offset[0]}px;\n'\
             + f'top: {match.offset[1] - parent.offset[1]}px;\n'\
             + f'width: {match.width}px;\n'\
             + f'height: {match.height}px\n'\
@@ -25,10 +35,10 @@ def listMatches(artboard,name):
     return matches
 
 def find(artboard, name, result_list):
-    print(artboard)
-    print(name)
+
     try:
         for layer in artboard:
+            print(layer)
             if layer.name == name:
                 result_list.append(layer)
             find(layer, name, result_list)
